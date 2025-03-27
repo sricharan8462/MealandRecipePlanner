7cf3194589e82a34d8acaae8e735f7eda88574f1
sricharan8462 <sricharan8462@yahoo.com>
2025-03-27 15:47:21 -0400

    Setup Recipe Details Screen structure

---
+++ lib/screens/recipe_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecipeDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Recipe Details')),
+      body: Center(
+        child: Text('Recipe details will be displayed here.'),
+      ),
+    );
+  }
+}
