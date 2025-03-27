ef8cb7b84fabcdbd6ab0cb5a0bfea7f281f85c20
srikrishnakolli <srikrishnakolli0@gmail.com>
2025-03-27 15:58:30 -0400

    Added dietary filters to Recipe List

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
 
-class RecipeListScreen extends StatelessWidget {
+class RecipeListScreen extends StatefulWidget {
+  @override
+  _RecipeListScreenState createState() => _RecipeListScreenState();
+}
+
+class _RecipeListScreenState extends State<RecipeListScreen> {
   final List<String> recipes = [
     "Vegan Salad",
     "Gluten-Free Pasta",
     "Chicken Stir Fry",
     "Mushroom Risotto"
   ];
+  
+  String selectedFilter = "All";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Recipes')),
-      body: ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(recipes[index]),
-            onTap: () => Navigator.pushNamed(
-              context,
-              '/recipe-details',
-              arguments: recipes[index], // Pass recipe name
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: DropdownButton<String>(
+              value: selectedFilter,
+              items: ["All", "Vegan", "Gluten-Free", "Non-Vegetarian"]
+                  .map((filter) => DropdownMenuItem(value: filter, child: Text(filter)))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedFilter = value!;
+                });
+              },
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(recipes[index]),
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    '/recipe-details',
+                    arguments: recipes[index],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
