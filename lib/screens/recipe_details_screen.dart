1a3a68561429d941c9dd36c3f070b5a015994f56
sricharan8462 <sricharan8462@yahoo.com>
2025-03-27 15:59:29 -0400

    Added ingredients and steps in Recipe Details Screen

--- lib/screens/recipe_details_screen.dart
+++ lib/screens/recipe_details_screen.dart
@@ -8,10 +8,25 @@ class RecipeDetailsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(recipeName)),
-      body: Center(
-        child: Text(
-          "Details for $recipeName",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Ingredients:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
+            Text('- Ingredient 1\n- Ingredient 2\n- Ingredient 3'),
+            SizedBox(height: 15),
+            Text(
+              'Steps:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
+            Text('1. Step one\n2. Step two\n3. Step three'),
+          ],
         ),
       ),
     );
